47c71ce2901fadac15e56114c6e4fa27efbcd102
Sergey Klimov <dcdarv@gmail.com>
2011-12-28 22:07:19 -0600

    fixed stuff for iPad

--- NerderyCodeChallenge/MSDetailViewController.m
+++ NerderyCodeChallenge/MSDetailViewController.m
@@ -85,7 +85,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    [(UIScrollView *) [self.movieDescription.subviews objectAtIndex:0] setAlwaysBounceVertical:NO];
+
     [self configureView];
 }
 
